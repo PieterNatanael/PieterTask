680449d46334cf96411889e763512dee15926796
PieterNatanael <pieternatanael@icloud.com>
2024-12-06 14:56:51 +0800

    Initial Commit

---
+++ PieterTask/PieterTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieterTaskApp.swift
+//  PieterTask
+//
+//  Created by Pieter Yoshua Natanael on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PieterTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PieterTaskTests/PieterTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  PieterTaskTests.swift
+//  PieterTaskTests
+//
+//  Created by Pieter Yoshua Natanael on 06/12/24.
+//
+
+import Testing
+@testable import PieterTask
+
+struct PieterTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
